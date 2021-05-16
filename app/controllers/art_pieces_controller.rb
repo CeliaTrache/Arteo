2d127a5ad4ec05e25a04f865be1c0094bb2b2555
julien loiseau <julien.loiseau@edu.escp.eu>
2021-05-16 10:41:24 +0200

    home page search bar

--- app/controllers/art_pieces_controller.rb
+++ app/controllers/art_pieces_controller.rb
@@ -3,7 +3,12 @@ class ArtPiecesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-    @art_pieces = policy_scope(ArtPiece).order(created_at: :desc)
+    if params[:query].present?
+      sql_query = "title ILIKE :query OR category ILIKE :query OR artist ILIKE :query"
+      @art_pieces = policy_scope(ArtPiece).where(sql_query, query: "%#{params[:query]}%")
+    else
+      @art_pieces = policy_scope(ArtPiece).order(created_at: :desc)
+    end
   end
 
   def show
