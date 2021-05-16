b0bd603ac97d48e38e8e9f6d4a8ab5bf4b2604dd
julien loiseau <julien.loiseau@edu.escp.eu>
2021-05-16 05:32:51 +0200

    pundit setup

---
+++ app/policies/booking_policy.rb
@@ -0,0 +1,33 @@
+class BookingPolicy < ApplicationPolicy
+  # Le cas 'index' n'est pas à couvrir
+  # class Scope < Scope
+  #   def resolve
+  #     scope.all
+  #   end
+  # end
+
+  def new?
+    true
+  end
+
+  def create?
+    new?
+  end
+
+  def destroy?
+    record.user == user
+  end
+
+  def update?
+    destroy?
+  end
+
+  def edit?
+    destroy?
+  end
+
+  def show?
+    destroy?
+  end
+
+end
