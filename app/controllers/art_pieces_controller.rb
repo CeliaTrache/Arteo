86b1d18168bb63bfdaa9c23a08223208392e1c54
vincentvinciguerra <vincent.vinciguerra@outlook.fr>
2021-05-17 17:34:42 +0200

    new-art-pieces

--- app/controllers/art_pieces_controller.rb
+++ app/controllers/art_pieces_controller.rb
@@ -1,5 +1,6 @@
 class ArtPiecesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :model :show
+  
+  skip_before_action :authenticate_user!, only: :index :show
 
   def index
     @art_pieces = ArtPiece.all
