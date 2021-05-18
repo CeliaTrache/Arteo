7be9efbf6e875dccf376b97dfe69282fc4fbde81
CeliaTrache <celiatrache@gmail.com>
2021-05-18 17:15:42 +0200

    page show is working

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'bookings/edit'
-  get 'bookings/update'
-  get 'bookings/destroy'
-  get 'bookings/show'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
