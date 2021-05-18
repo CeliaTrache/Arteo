e53ee1d7705af19b471546c603afd8449d38b0f3
CeliaTrache <celiatrache@gmail.com>
2021-05-18 12:02:12 +0200

    modifying the seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_17_132606) do
+ActiveRecord::Schema.define(version: 2021_05_17_132607) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,18 @@ ActiveRecord::Schema.define(version: 2021_05_17_132606) do
     t.index ["user_id"], name: "index_art_pieces_on_user_id"
   end
 
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_time"
+    t.date "end_time"
+    t.integer "price"
+    t.bigint "art_piece_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["art_piece_id"], name: "index_bookings_on_art_piece_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -43,4 +55,6 @@ ActiveRecord::Schema.define(version: 2021_05_17_132606) do
   end
 
   add_foreign_key "art_pieces", "users"
+  add_foreign_key "bookings", "art_pieces"
+  add_foreign_key "bookings", "users"
 end
