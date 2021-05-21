5c930dec9bc8c01feff68b2021eb7497185b5073
CeliaTrache <celiatrache@gmail.com>
2021-05-21 15:57:17 +0200

    update seed with review delete all

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Review.delete_all
 Booking.delete_all
 ArtPiece.delete_all
 User.delete_all
